917ff48537fca52cee0099cafdf42c96794a3ff9
Bill Gooch <billgch@gmail.com>
2015-04-18 15:18:53 +0100

    Updated ....

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -7,12 +7,50 @@
 
     function Config($routeProvider) {
 
-        $routeProvider.when('/widgets', { templateUrl: 'app/dashboard/dashBoard.html'})
-                      .when('/dashboard', {
-                                templateUrl: 'app/dashboard/gridsterDemo/view.html',
+        $routeProvider.when('/index', { templateUrl: 'index.html'})
+                      .when('/h2FuelCell', {
+                                templateUrl: 'app/dashboard/gridsterDemo/h2FuelCell.html',
                                 controller: 'DashboardCtrl'
-                            });
-        $routeProvider.otherwise({redirectTo: '/widgets'});
+                            })
+                        .when('/costSavings', {
+                            templateUrl: 'view/h2fuelCell/templateUrl.html',
+                            controller: 'DashboardCtrl'
+                        })
+                        .when('/guarantee', {
+                            templateUrl: 'view/h2fuelCell/guarantee.html',
+                            controller: 'DashboardCtrl'
+                        })
+                        .when('/insurance', {
+                            templateUrl: 'view/h2fuelCell/insurance.html',
+                            controller: 'DashboardCtrl'
+                        })
+                        .when('/lifeCycle', {
+                            templateUrl: 'view/h2fuelCell/lifeCycle.html',
+                            controller: 'DashboardCtrl'
+                        })
+                        .when('/technology', {
+                            templateUrl: 'view/h2fuelCell/technology.html',
+                            controller: 'DashboardCtrl'
+                        })
+                        .when('/warranty', {
+                            templateUrl: 'view/h2fuelCell/warranty.html',
+                            controller: 'DashboardCtrl'
+                        })
+                        .when('/aboutUs', {
+                            templateUrl: 'view/aboutUs.html',
+                            controller: 'DashboardCtrl'
+                        })
+                        .when('/blog', {
+                            templateUrl: 'view/blog.html',
+                            controller: 'DashboardCtrl'
+                        })
+                        .when('/contact', {
+                            templateUrl: 'view/contact.html',
+                            controller: 'DashboardCtrl'
+                        })
+
+
+        $routeProvider.otherwise({redirectTo: '/index'});
 
     }
 
@@ -20,22 +58,16 @@
     //   APP MODULE SET UP
     //--------------------------
 
-    angular.module("rtShell", [
+    angular.module("h2TechApp", [
             "ngRoute",
             "ui.bootstrap",
-            'xeditable',
-            'gridster'
+            "ngResource"
             ])
            .config(Config)
-           .run(function (editableOptions, editableThemes,CONFIG,rtConfig) {
+           .run(function (h2Config) {
 
-                rtConfig.setConfigData(CONFIG);
-                editableOptions.theme = 'default';
-                // overwrite submit button template
-                editableThemes['default'].submitTpl = '<a  href="javascript:void(0)" class="btn btn-mini btn-inverse" style="margin: 0 0 2px 2px;" ng-click="$form.$submit()"><span class="icon-ok icon-white"></span></a>';
-                editableThemes['default'].cancelTpl = '<a  href="javascript:void(0)" class="btn btn-mini btn-inverse" style="margin: 0 2px 2px 2px;" ng-click="$form.$cancel()"><span class="icon-remove"></span></a>';
-
-    });
+                    h2Config.setConfigData(CONFIG);
+              });
 
     fetchConfigData().then(bootstrapApplication);
 
@@ -43,8 +75,8 @@
         var initInjector = angular.injector(["ng"]);
         var $http = initInjector.get("$http");
 
-        return $http.get("assets/config/refdata.json").then(function(response) {
-            angular.module("rtShell").constant("CONFIG", response.data);
+        return $http.get("config/h2Config.json").then(function(response) {
+            angular.module("h2TechApp").constant("CONFIG", response.data);
         }, function(errorResponse) {
             // Handle error case
         });
@@ -52,7 +84,7 @@
 
     function bootstrapApplication() {
         angular.element(document).ready(function() {
-            angular.bootstrap(document, ["rtShell"]);
+            angular.bootstrap(document, ["h2TechApp"]);
         });
     }
 
